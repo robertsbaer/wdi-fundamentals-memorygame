3a87e25731bc984d3c47faa375bdfcb867596744
robertsbaer <robertsbaer@yahoo.com>
2019-11-06 11:40:34 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,32 @@
 console.log("Up and running!");
 
-let cards = ["queen", "queen", "king", "king"];
 
-let cardsInPlay = [];
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = cards[0];
-var cardTwo = cards[3];
+var cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	};
+};
 
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
-	}
-}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
 
+flipCard(0);
 
-console.log("User fliped " + cardsInPlay[0]);
-console.log("User fliped " + cardsInPlay[1]);
+flipCard(2);
 
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!"); 
+} else {
+	alert("Sorry, try again.");
+} 
